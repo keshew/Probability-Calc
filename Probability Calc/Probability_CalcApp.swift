d70af94f5e7be91b0c91d7a6b03f4885b5bd5217
keshew <artyom.korotckov2017@yandex.ru>
2025-12-12 17:19:43 +0500

    Initial Commit

---
+++ Probability Calc/Probability_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Probability_CalcApp.swift
+//  Probability Calc
+//
+//  Created by Артём Коротков on 12.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Probability_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
